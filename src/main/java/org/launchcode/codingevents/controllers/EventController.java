a1b1f96fbfb751c39295ab6fd47dfdc435f29284
vaishnavi <vaishu.srini15@gmail.com>
2021-09-08 15:14:54 -0500

    my first commit

---
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -0,0 +1,32 @@
+package org.launchcode.codingevents.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("events")
+public class EventController {
+    @GetMapping
+    public String displayAllEvents(Model model) {
+        List<String> events = new ArrayList<>();
+        events.add("Enjoy Coding");
+        events.add("Spring Loop");
+        events.add("SpringOne Platform");
+        events.add("Apple WWDC");
+        events.add("Code With Pride");
+        model.addAttribute("events", events);
+        return "events/index";
+    }
+
+    //lives at events/create
+    @GetMapping("create")
+    public String displayCreateEventForm() {
+        return "events/create";
+    }
+
+}
