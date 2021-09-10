2de816b0ffe8a4ecf7bb6734e15bcd6ccaf1832f
vaishnavi <vaishu.srini15@gmail.com>
2021-09-09 22:29:40 -0500

    Thymeleaf Exercise

---
+++ src/main/java/org/launchcode/codingevents/controllers/Event.java
@@ -0,0 +1,31 @@
+package org.launchcode.codingevents.controllers;
+
+public class Event {
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public String getEventDescription() {
+        return eventDescription;
+    }
+
+    public void setEventDescription(String eventDescription) {
+        this.eventDescription = eventDescription;
+    }
+
+    public String getEventAddress() {
+        return eventAddress;
+    }
+
+    public void setEventAddress(String eventAddress) {
+        this.eventAddress = eventAddress;
+    }
+
+    private String eventName;
+    private String eventDescription;
+    private String eventAddress;
+}
--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -8,16 +8,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
-    private static List<String>events = new ArrayList<>();
+    private static List<String> events = new ArrayList<>();
+    private static Map<String,String> eventsMap = new HashMap<>();
+    private static List<Event> eventObjects = new ArrayList<>();
+
+
+//    @GetMapping
+//    public String displayAllEvents(Model model) {
+//        model.addAttribute("events", events);
+//        return "events/index";
+//    }
 
     @GetMapping
-    public String displayAllEvents(Model model) {
-        model.addAttribute("events", events);
+    public String displayAllEvents(Model model){
+       // Map<String,String> eventsMap=new HashMap<>();
+
+     /*   for (String event:events) {
+            eventsMap.put(event,event+" saint louis");
+        }
+        */
+
+   //     eventsMap.put("Menteaship","A fun meetup for connecting with mentors");
+      //  eventsMap.put("Code With Pride","A fun meetup sponsored by LaunchCode");
+      //  eventsMap.put("Javascripty", "An imaginary meetup for Javascript developers");
+//        model.addAttribute("events",eventsMap);
+        model.addAttribute("events",eventObjects);
         return "events/index";
     }
 
@@ -29,9 +51,18 @@ public class EventController {
 
     //lives at events/create
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName){
-        events.add(eventName);
+    public String createEvent(@RequestParam String eventName,@RequestParam String eventDescription,@RequestParam String eventAddress){
+        System.out.println("eventName");
+      //  events.add(eventName);
+        Event event = new Event();
+        event.setEventName(eventName);
+        event.setEventDescription(eventDescription);
+        event.setEventAddress(eventAddress);
+        eventObjects.add(event);
+        eventsMap.put(eventName,eventDescription);
+
         return "redirect: ";
     }
+
 
 }
